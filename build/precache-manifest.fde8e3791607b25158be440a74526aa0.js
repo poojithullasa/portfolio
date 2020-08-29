fffafde18734f907c7eaf9f54a8ee4feaa6e23bd
poojithullasa <upsy.0515@gmail.com>
2020-08-29 14:54:44 +0530

    Updated Portfolio Links

---
+++ build/precache-manifest.fde8e3791607b25158be440a74526aa0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913713cf756ade2ba9e85dc60def32a7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cfa59bfdfb303c53bd26",
+    "url": "/portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b33532daf8642b95c74a",
+    "url": "/portfolio/static/css/main.be750141.chunk.css"
+  },
+  {
+    "revision": "cfa59bfdfb303c53bd26",
+    "url": "/portfolio/static/js/2.6a4a640e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.6a4a640e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b33532daf8642b95c74a",
+    "url": "/portfolio/static/js/main.caecaee3.chunk.js"
+  },
+  {
+    "revision": "e68e86a61dc4cb5c2d58",
+    "url": "/portfolio/static/js/runtime-main.08e7a8af.js"
+  },
+  {
+    "revision": "05292fb36747fe15486f522c069abf72",
+    "url": "/portfolio/static/media/pic.05292fb3.jpg"
+  }
+]);
