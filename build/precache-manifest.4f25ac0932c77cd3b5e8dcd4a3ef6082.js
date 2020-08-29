72f4aec597f929a91bd91690b525ecf369208d96
poojithullasa <upsy.0515@gmail.com>
2020-08-29 15:03:58 +0530

    Uploaded build files

---
+++ build/precache-manifest.4f25ac0932c77cd3b5e8dcd4a3ef6082.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2cea6e61dc20825b8ecc3c3748dad0e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cfa59bfdfb303c53bd26",
+    "url": "/portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e5a5cda623cefdc08e0e",
+    "url": "/portfolio/static/css/main.be750141.chunk.css"
+  },
+  {
+    "revision": "cfa59bfdfb303c53bd26",
+    "url": "/portfolio/static/js/2.6a4a640e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.6a4a640e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5a5cda623cefdc08e0e",
+    "url": "/portfolio/static/js/main.b79633b4.chunk.js"
+  },
+  {
+    "revision": "e68e86a61dc4cb5c2d58",
+    "url": "/portfolio/static/js/runtime-main.08e7a8af.js"
+  },
+  {
+    "revision": "05292fb36747fe15486f522c069abf72",
+    "url": "/portfolio/static/media/pic.05292fb3.jpg"
+  }
+]);
